035dfcb6af8a0f0bdc97607cedff0f6ca9f5b814
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-13 23:42:46 +0530

    temp rebacked

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "https://trackitright-backend.onrender.com";
+export const API_BASE_URL = "http://localhost:8080";
+
+// export const API_BASE_URL = "https://trackitright-backend.onrender.com";
