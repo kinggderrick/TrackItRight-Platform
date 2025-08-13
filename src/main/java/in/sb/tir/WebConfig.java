88c7b2ffa8dd51b2b37a002f38b9d9b2776bd007
Sayyad Aslam <sayyadaslam2020@gmail.com>
2025-08-13 21:48:44 +0530

    added

--- src/main/java/in/sb/tir/WebConfig.java
+++ src/main/java/in/sb/tir/WebConfig.java
@@ -13,8 +13,8 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://track-it-right-platform.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
